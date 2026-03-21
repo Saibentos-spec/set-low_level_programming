4df947bc6e114201a96cc72f605e5340859a7865
SAIBA MOISE <saibentos@gmail.com>
2026-03-21 15:07:41 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int len = 0;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len / 2)
+	{
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+		i++;
+	}
+}
