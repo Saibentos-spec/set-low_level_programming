b6781400fbfaa9b05ebe51a0488c9149de8ff3b8
SAIBA MOISE <saibentos@gmail.com>
2026-03-30 10:15:51 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (0);
+}
