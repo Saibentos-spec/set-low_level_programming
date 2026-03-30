e8c93f3d4033e5a918f3dbf0fb6c478a5ca70af5
SAIBA MOISE <saibentos@gmail.com>
2026-03-30 18:15:45 +0200

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i])
+		i++;
+
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
