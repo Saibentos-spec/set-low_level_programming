be1452f2899b38e61095bf76d02d67653af99cb6
SAIBA MOISE <saibentos@gmail.com>
2026-03-30 18:45:33 +0200

    Create main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Task 1 */
+void print_number(int n);
+
+/* Task 3 */
+int _atoi(char *s);
+
+/* Task 4 */
+int is_number(char *s);
+
+#endif
