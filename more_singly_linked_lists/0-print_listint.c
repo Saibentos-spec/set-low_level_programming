7fe1ee0594ff019617669c98dfaf8c41cfcba6b7
SAIBA MOISE <saibentos@gmail.com>
2026-05-17 22:45:39 +0200

    Update 0-print_listint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
