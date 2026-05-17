e084f80d70a9f7f5622b53842eb3f36c6f880dcd
SAIBA MOISE <saibentos@gmail.com>
2026-05-17 22:39:41 +0200

    Create 6-pop_listint.c

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	temp = *head;
+	n = temp->n;
+	*head = temp->next;
+
+	free(temp);
+
+	return (n);
+}
