3378fe28f8aae0dfc289e7bb2e7f58d8542cf4b8
SAIBA MOISE <saibentos@gmail.com>
2026-05-17 22:05:08 +0200

    Update 4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
 
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+
+		free(head->str);
+		free(head);
+
+		head = temp;
+	}
+}
