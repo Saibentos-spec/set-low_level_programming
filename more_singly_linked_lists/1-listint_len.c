db51e7dbe6e5f78463df4d4c9d1ee5709db35d16
SAIBA MOISE <saibentos@gmail.com>
2026-05-17 22:47:06 +0200

    Update 1-listint_len.c

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - returns number of elements in a listint_t list
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
