6722ece379d6a528c581ede4a7fcde271d59b6fd
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 12:41:29 +0200

    Update 102-free_listint_safe.c

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -9,69 +9,15 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *visited[1024];
 	size_t count = 0;
-	listint_t *current, *next;
-	listint_t *slow, *fast;
+	size_t size;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	slow = *h;
-	fast = *h;
-
-	/* Detect loop */
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			break;
-	}
-
-	current = *h;
-	if (slow == fast && fast != NULL) /* Loop detected */
-	{
-		/* Find loop start */
-		slow = *h;
-		while (slow != fast)
-		{
-			slow = slow->next;
-			fast = fast->next;
-		}
-
-		/* Free up to loop */
-		while (current != slow)
-		{
-			next = current->next;
-			free(current);
-			current = next;
-			count++;
-		}
-
-		/* Free the loop nodes once */
-		next = current->next;
-		free(current);
-		count++;
-		while (next != current)
-		{
-			listint_t *temp = next;
-			next = next->next;
-			free(temp);
-			count++;
-		}
-	}
-	else
-	{
-		/* No loop, free normally */
-		while (current != NULL)
-		{
-			next = current->next;
-			free(current);
-			current = next;
-			count++;
-		}
-	}
+	size = free_nodes(*h, visited, &count);
 
 	*h = NULL;
-	return (count);
+	return (size);
 }
