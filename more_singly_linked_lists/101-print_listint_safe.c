6e0528c047bb0c8466c8da6c89dc5d7965ecf521
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 12:06:41 +0200

    Update 101-print_listint_safe.c

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list (safe version)
+ * print_listint_safe - prints a listint_t linked list safely (handles loops)
  * @head: pointer to the head of the list
  *
  * Return: number of nodes in the list
@@ -11,12 +11,14 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *slow = head;
 	const listint_t *fast = head;
+	const listint_t *loop_start = NULL;
 	size_t count = 0;
-	int loop_detected = 0;
+	size_t i;
 
 	if (head == NULL)
 		return (0);
 
+	/* Detect loop using Floyd's algorithm */
 	while (fast && fast->next)
 	{
 		slow = slow->next;
@@ -24,26 +26,33 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (slow == fast)
 		{
-			loop_detected = 1;
+			/* Loop detected - find start of loop */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			loop_start = slow;
 			break;
 		}
 	}
 
+	/* Print the list */
 	slow = head;
-	count = 0;
-
-	while (head)
+	while (slow)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p] %d\n", (void *)slow, slow->n);
 		count++;
 
-		if (loop_detected && head->next == slow && count > 1)
+		/* If we are about to print the node that starts the loop again */
+		if (loop_start && slow->next == loop_start && count > 1)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)slow->next, slow->next->n);
 			break;
 		}
 
-		head = head->next;
+		slow = slow->next;
 	}
 
 	return (count);
