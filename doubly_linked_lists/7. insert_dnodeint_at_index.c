73ccf15dfaea9eb849ed5a81099714a4ab97f9d3
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 12:58:31 +0200

    Create 7. insert_dnodeint_at_index.c

---
+++ doubly_linked_lists/7. insert_dnodeint_at_index.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *new_node, *temp;
+    unsigned int i = 0;
+
+    if (h == NULL)
+        return (NULL);
+
+    if (idx == 0)
+        return (add_dnodeint(h, n));
+
+    temp = *h;
+
+    while (temp != NULL && i < idx - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+
+    new_node->next = temp->next;
+    new_node->prev = temp;
+
+    if (temp->next != NULL)
+        temp->next->prev = new_node;
+
+    temp->next = new_node;
+
+    return (new_node);
+}
