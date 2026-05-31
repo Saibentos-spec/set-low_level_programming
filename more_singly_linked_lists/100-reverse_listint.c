988db259deaecfbf1d14dd1dadaef7c6f5663e17
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 12:00:17 +0200

    Update 100-reverse_listint.c

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * reverse_listint - reverses a listint_t linked list
- * @head: pointer to pointer to the head
+ * @head: pointer to pointer to the head of the list
  *
  * Return: pointer to the first node of the reversed list
  */
@@ -10,16 +10,16 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
 	listint_t *current = *head;
-	listint_t *next = NULL;
 
-	while (current != NULL)
+	while (current)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		*head = current->next;        /* Move head to next node */
+		current->next = prev;         /* Reverse the link */
+		prev = current;               /* Move prev forward */
+		current = *head;              /* Move current forward */
 	}
 
-	*head = prev;
+	*head = prev;   /* Update head to new first node */
+
 	return (*head);
 }
