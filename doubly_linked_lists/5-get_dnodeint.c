3033b1b5c825119ada59776e0cfb0fb52e91d505
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 13:30:05 +0200

    Update 5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,15 +1,24 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - returns node at index
+ * @head: head of list
+ * @index: index to search
+ *
+ * Return: pointer to node or NULL
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (head != NULL)
-    {
-        if (i == index)
-            return (head);
-        head = head->next;
-        i++;
-    }
-    return (NULL);
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
 }
