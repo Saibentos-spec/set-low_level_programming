7adbf5c2c8a2d5a7a62ee69c557f709e6be7e1e5
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 13:33:18 +0200

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,40 +1,47 @@
 #include "lists.h"
 
+/**
+ * delete_dnodeint_at_index - deletes node at index
+ * @head: pointer to head
+ * @index: index to delete
+ *
+ * Return: 1 success, -1 failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp;
-    unsigned int i = 0;
+	dlistint_t *temp;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    temp = *head;
+	temp = *head;
 
-    if (index == 0)
-    {
-        *head = temp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(temp);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
 
-    while (temp != NULL && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
+	while (temp != NULL && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-    if (temp == NULL)
-        return (-1);
+	if (temp == NULL)
+		return (-1);
 
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
 
-    if (temp->prev != NULL)
-        temp->prev->next = temp->next;
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
 
-    free(temp);
+	free(temp);
 
-    return (1);
+	return (1);
 }
