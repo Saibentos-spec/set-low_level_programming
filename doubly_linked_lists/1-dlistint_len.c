b05db90924b2425bf7df4fc28f234bbfc5585886
SAIBA MOISE <saibentos@gmail.com>
2026-05-31 13:25:57 +0200

    Update 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,20 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - returns number of elements in a dlistint_t list
+ * @h: pointer to head
+ *
+ * Return: number of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-    return (count);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
